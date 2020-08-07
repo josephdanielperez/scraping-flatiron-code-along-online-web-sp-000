60b4bc125e93f74e04210ba192f53f21141f3ab8
Joseph Perez <hello@josephdanielperez.com>
2020-08-07 02:57:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,9 +8,9 @@ class Scraper
   def get_page
 
     def get_page
-   
+
       Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-   
+
       #doc.css(".post").each do |post|
         #course = Course.new
         #course.title = post.css("h2").text
