7fc0d8d38f84e250b7d2a836d3fb717b5371eb2f
Joseph Perez <hello@josephdanielperez.com>
2020-08-07 02:59:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,5 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 require_relative './course.rb'
 
@@ -16,6 +15,17 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
+  end
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
   end
 
 end
